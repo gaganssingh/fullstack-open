3577f04240f6045d30a588cf26d8788b8d2381a0
Gagan Singh <gagansingh.dev@outlook.com>
2020-07-18 17:31:49 -0400

    Finished Ex. 2.7

--- part2/the-phonebook/src/App.js
+++ part2/the-phonebook/src/App.js
@@ -6,6 +6,11 @@ const App = () => {
 
    const addPerson = (event) => {
       event.preventDefault();
+      if (persons.find((person) => person.name === newName)) {
+         alert(`${newName} is already added to the phonebook`);
+         return;
+      }
+
       const personObject = { name: newName };
       setPersons(persons.concat(personObject));
       setNewName("");
